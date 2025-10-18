80f5e3a67d5786240033aa21372aa69aadff2855
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-18 18:30:00 -0400

    feat: finish 06 js exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+// create a condition that returns Error if num is neg
+if (num < 0) {
+    return "ERROR"
+}
+// create a variable to hold the string to be returned
+let finalString = "";
+// loop through the string a num amount of times
+for (let i = 0;i<num;i++){
+// for each iteration of that loop concatenate the string
+finalString += string
+}
+// return the string with the concatenation
+return finalString;
 };
 
 // Do not edit below this line
