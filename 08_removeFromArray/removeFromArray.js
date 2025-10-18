9e0363437b50a75d49ab82d7f9113c3f6003a5d5
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-18 18:30:00 -0400

    feat: do a hacky solution that works somehow

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = (arr,...theRemovedItems)=> {
+    // This solution was described as "brute force" by chatGPT lmao
+    // for (item of arr) {
+    //     for (removedItem of theRemovedItems) {
+    //         for (item of arr) {
+    //             for (removedItem of theRemovedItems) {
+    //                 if (item === removedItem) {
+    //                     const indexOfRemoved = arr.indexOf(item);
+    //                     arr.splice(indexOfRemoved,1);
+    //                 }
+    //             }
+    //         } 
+    //     }
+    // } return arr;
 };
 
 // Do not edit below this line
