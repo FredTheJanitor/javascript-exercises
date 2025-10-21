b62dc139ae1e969ffabc4254605b3f24565c9eac
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-21 18:30:00 -0400

    feat: add if else and tighter boolean ver for 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = (year)=>{
+    return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0);
 
+    // basically main over complication below was not letting the
+    // boolean operators just return the boolean.
+
+    
+    
+    // // leap years dont include multiples of 100 unless they
+    // // are multiples of 400
+    // if (year % 4 === 0) {
+    //     if (year % 100 === 0) {
+    //         // if (year % 400 === 0) {
+    //         //     return true;
+    //         // } else {
+    //         //     return false;
+    //         // }
+    //         return (year%400===0)? true : false
+    //     }
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 };
 
 // Do not edit below this line
