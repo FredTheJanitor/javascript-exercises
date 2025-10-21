38babdec150f980177fb753ec7ec353ddbfaae22
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-21 18:30:00 -0400

    feat: do lesson 11 JS Exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = (fTemp)=>{
+  const cTemp = (fTemp - 32)/(9/5);
+  cTempPretty = Math.round(cTemp*10)/10;
+  return cTempPretty;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (cTemp)=>{
+  const fTemp = (cTemp*1.8)+32;
+  fTempPretty = Math.round(fTemp*10)/10;
+  return fTempPretty;
 };
 
 // Do not edit below this line
