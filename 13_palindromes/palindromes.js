b98b2548f32c76881648868d13747e4cfd8860c0
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-26 18:30:00 -0500

    feat: make sure punctuation is removed from og too

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const input = string
+        .split("")
+        .filter((char)=>char.toLowerCase() !== char.toUpperCase())
+        .join("");
+
+    return input === string
+        .split("")
+        .reverse()
+        .filter((char)=>char.toLowerCase() !== char.toUpperCase())
+        .join("");
 
 };
 
