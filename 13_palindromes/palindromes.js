76e07de4dc893f3bb7c8210acd45514c27bd2ff7
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-26 18:30:00 -0500

    feat: add validation to match test environment

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -2,14 +2,18 @@ const palindromes = function (string) {
     const input = string
         .toLowerCase()
         .split("")
-        .filter((char)=>char.toLowerCase() !== char.toUpperCase())
+        // check if the character is a letter by seeing if it has cases, or a number
+        .filter((char)=>(char.toLowerCase() !== char.toUpperCase()) || isFinite(char))
+        // I guess spaces are finite, so yeet those
+        .filter((char)=>char !== " ")
         .join("");
 
     return input === string
         .toLowerCase()
         .split("")
         .reverse()
-        .filter((char)=>char.toLowerCase() !== char.toUpperCase())
+        .filter((char)=>(char.toLowerCase() !== char.toUpperCase()) || isFinite(char))
+        .filter((char)=>char !== " ")
         .join("");
 
 };
