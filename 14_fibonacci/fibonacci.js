24c887731b18950556625a02d61ad2de2d46f99d
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-27 18:30:00 -0500

    feat: get it fibbonaccin' knowmsayyyyyn

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    const fibArr = [];
+    const z = +x;
+    if (z===0) {
+        return 0;
+    } else if (z < 0) {
+        return "OOPS";
+    } else if (z > 1000) {
+        return 'please dont break my computer lol';
+    }
+    for(let i=1;i<=z;i++){
+        if (i === 1) {
+            fibArr.push(1);
+        } else if (i === 2) {
+            fibArr.push(1);
+        } else if (i > 2) {
+            fibArr.push(fibArr[fibArr.length-1] + fibArr[fibArr.length-2]);
+        }
+    }
+    return fibArr[fibArr.length-1];
+    
 
 };
 
