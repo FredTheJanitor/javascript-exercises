35ec88a9b0877a47f1df393cbd82baa2b6e9e826
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-27 18:30:00 -0500

    feat: finish TOP JS exercises like a boss

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const arrayWithAge = array.map(person=>{
+        if (person.yearOfDeath == undefined) {
+            person["yearOfDeath"] = new Date().getFullYear()
+        }
+        const calcAge = person.yearOfDeath-person.yearOfBirth;
+        return {...person, age: calcAge}
+    });
+    const arraywithAgeSorted = arrayWithAge.sort((a,b)=>a.age-b.age);
+    const oldestPerson = arraywithAgeSorted.at(-1);
+    return oldestPerson;
 };
 
 // Do not edit below this line
